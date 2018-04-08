2e2431a4268547ac51d742e59bd8fb8d56f7fb0e
Alexey Vorobyov <a.vorobjovs@sehner.international>
2018-04-08 20:30:52 +0300

    Sequlize USER model added

--- server/src/app.js
+++ server/src/app.js
@@ -1,30 +1,28 @@
 const express = require('express')
 // const path = require('path')
+// const serveStatic = require('serve-static')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-// const serveStatic = require('serve-static')
+const {sequelize} = require('./models')
+const config = require('./config')
 
-const PORT = process.env.PORT || 8081
+const PORT = config.PORT
 const app = express()
 
 // logging into console
 app.use(morgan('combined'))
-
 // auto parsing json requests
 app.use(bodyParser.json())
-
 // cross origin requests are allowed to that app
 app.use(cors())
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
-  })
-})
+require('./routes')(app)
 
-// app.use(serveStatic(path.join(__dirname, 'client/src')))
-// app.use(serveStatic(`../../client/src`))
-app.listen(PORT, () => {
-  console.log(`Server is up on ${PORT}`)
+sequelize.sync().then(() => {
+  // app.use(serveStatic(path.join(__dirname, 'client/src')))
+  // app.use(serveStatic(`../../client/src`))
+  app.listen(PORT, () => {
+    console.log(`Server is up on ${PORT}`)
+  })
 })
